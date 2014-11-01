853a64f5b3af0e8f08f70d1c91c2a34b53a18634
Andy Aparece <andy.aparece@gmail.com>
2014-11-02 04:00:38 +0800

    Add Pricing page

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/how-we-work', function(req, res) {
   res.render('how-we-work.jade', { title: 'How we work', id: 'how-we-work'});
 });
 
+router.get('/pricing', function(req, res) {
+  res.render('pricing.jade', { title: 'Pricing', id: 'pricing'});
+});
+
 module.exports = router;
