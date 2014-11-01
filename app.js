ddd91a5240558a641cab0941634a38e6da932fc0
Andy Aparece <andy.aparece@gmail.com>
2014-11-02 02:52:33 +0800

    Add How we work page

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-
 app.use('/', routes);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'optiBPO - optimising business outsourcing' , id: 'homepage'});
+});
+
+router.get('/how-we-work', function(req, res) {
+  res.render('how-we-work.jade', { title: 'How we work', id: 'how-we-work'});
 });
 
 module.exports = router;
