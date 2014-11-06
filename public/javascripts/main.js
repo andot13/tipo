49370d0aa425ab8f24e448c14d2183051d7ce4d0
Andy Aparece <andy.aparece@gmail.com>
2014-11-06 19:56:39 +0800

    Remove animation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
-	$('.animate-fadein-left').addClass("hidden-elem").viewportChecker({
-		classToAdd: 'visible-elem animated fadeInLeft',
-		offset: 100,
-		repeat: true
-	});
-
-	$('.animate-fadein-right').addClass("hidden-elem").viewportChecker({
-		classToAdd: 'visible-elem animated fadeInRight',
-		offset: 100,
-		repeat: true
-	});
-
-	$('.animate-fadein-down').addClass("hidden-elem").viewportChecker({
-		classToAdd: 'visible-elem animated fadeInDown',
-		offset: 100,
-		repeat: true
-	});
-
-	$('.animate-fadein').addClass("hidden-elem").viewportChecker({
-		classToAdd: 'visible-elem animated fadeIn',
-		offset: 100,
-		repeat: true
-	});
-
-
 	//Contact Form
 	$("#contact-us-form").submit(function()
 	{
