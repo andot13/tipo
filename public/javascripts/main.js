90e98b105796f93fa8326cd6501b86b5b804945b
Andy Aparece <andy.aparece@gmail.com>
2014-11-07 15:58:43 +0800

    Add contact form validation and some revision as per email

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,44 +1,29 @@
 $(document).ready(function() {
-	//Contact Form
-	$("#contact-us-form").submit(function()
-	{
-		var email = $("#email").val(); // get email field value
-		var name = $("#name").val(); // get name field value
-		var message = $("#message").val(); // get message field value
-		$.ajax(
-		{
-			type: "POST",
-			url: "https://mandrillapp.com/api/1.0/messages/send.json",
-			data: {
-				'key': 'yBkqCWXGZL4qhvesUtqymQ',
-				'message': {
-					'from_email': email,
-					'from_name': name,
-					'headers': {
-						'Reply-To': email
-					},
-					'subject': 'optiBPO Contact Form Submission',
-					'text': message,
-					'to': [
-					{
-						'email': 'andotjackass@gmail.com',
-						'name': 'Andy',
-						'type': 'to'
-					}]
-				}
-			}
-		})
-		.done(function(response) {
-			alert('Your message has been sent. Thank you!'); // show success message
-			$("#name").val(''); // reset field after successful submission
-			$("#email").val(''); // reset field after successful submission
-			$("#message").val(''); // reset field after successful submission
-		})
-		.fail(function(response) {
-			alert('Error sending message.');
-		});
-		return false; // prevent page refresh
-	});
 
+//form validation rules
+    $("#contact-us-form").validate({
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "Please enter your name",
+            email: "Oops! Please provide a valid email address"
+        },
+        errorElement: "div",
+        wrapper: "div",  // a wrapper around the error message
+        errorPlacement: function(error, element) {
+         	element.before(error);
+	        offset = element.offset();
+	        error.addClass('message');  // add a class to the wrapper
+	        offset = element.offset();
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var mandrill = require('mandrill-api');
+var mandrill_client = new mandrill.Mandrill('yBkqCWXGZL4qhvesUtqymQ'); 
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -23,6 +24,60 @@ router.get('/contact-us', function(req, res) {
 	res.render('contact-us.jade', { title: 'Contact us', id: 'contact-us'});
 });
 
+router.post('/contact-us', function(req, res) {
+	req.assert('name', 'Please enter you name.').notEmpty();
+	req.assert('email', 'Please provide a valid email').isEmail();  
+
+	var errors = req.validationErrors();  
+	if( !errors){   //No errors were found.  Passed Validation!
+		res.render('contact-us.jade', { 
+			title: 'Contact us',
+			message: 'Thank you! Your message has been sent!',
+			id: 'contact-us',
+			successClass : 'success',
+			errors: {}
+		});
+
+			var name = req.body.name;
+			var email = req.body.email;
+			var message = req.body.message;
+
+			var message = {
+			    "text": message,
+			    "subject": "optiBPO Contact Form Submission",
+			    "from_email": email,
+			    "from_name": name,
+			    "to": [{
+			            "email": "andotjackass@gmail.com",
+			            "name": "Andy"
+			        }],
+			    "headers": {
+			        "Reply-To": email
+			    },
+			    "track_opens": true,
+			    "track_clicks": true,
+			    "auto_text": true
+			};
+
+			mandrill_client.messages.send({"message": message}, function(result) {
+		    console.log(result);
+		   
+			}, function(e) {
+			    // Mandrill returns the error as an object with name and message keys
+			    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+			    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+			});
+	}
+	else {   //Display errors to user
+		res.render('contact-us.jade', { 
+			title: 'Contact us',
+			message: 'There seems to be an error. Please try again',
+			id: 'contact-us',
+			message: 'Error!',
+			errors: errors
+		});
+	}
+});
 
 
 module.exports = router;
