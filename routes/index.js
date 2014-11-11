0971747d567ab393b54af51fc97f76101f345004
Andy Aparece <andy.aparece@gmail.com>
2014-11-11 10:48:24 +0800

    Fix contact us form

--- routes/index.js
+++ routes/index.js
@@ -24,27 +24,27 @@ router.get('/contact-us', function(req, res) {
 	res.render('contact-us.jade', { title: 'Contact us', id: 'contact-us'});
 });
 
-router.post('/contact-send', function(req, res) {
-	req.assert('name', 'Please enter you name.').notEmpty();
-	req.assert('email', 'Please provide a valid email').isEmail();  
+router.post('/contact-us', function(req, res) {
+	// req.assert('name', 'Please enter you name.').notEmpty();
+	// req.assert('email', 'Please provide a valid email').isEmail();  
 
-	var errors = req.validationErrors();  
-	if( !errors){   //No errors were found.  Passed Validation!
-		res.render('contact-us.jade', { 
-			title: 'Contact us',
-			message: 'Thank you! Your message has been sent!',
-			id: 'contact-us',
-			successClass : 'success',
-			errors: {}
-		});
+	// var errors = req.validationErrors();  
+	// if( !errors){   //No errors were found.  Passed Validation!
+	// 	res.send({ 
+	// 		title: 'Contact us',
+	// 		message: 'Thank you! Your message has been sent!',
+	// 		id: 'contact-us',
+	// 		successClass : 'success',
+	// 		errors: {}
+	// 	});
 
 			var name = req.body.name;
 			var email = req.body.email;
-			var message = req.body.message;
+			var formMessage = req.body.message;
 
 			var message = {
-			    "text": message,
-			    "subject": "optiBPO Contact Form Submission",
+			    "text": formMessage,
+			    "subject": "A message via optiBPO Contact Form",
 			    "from_email": email,
 			    "from_name": name,
 			    "to": [{
@@ -67,16 +67,16 @@ router.post('/contact-send', function(req, res) {
 			    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
 			    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
 			});
-	}
-	else {   //Display errors to user
-		res.render('contact-us.jade', { 
-			title: 'Contact us',
-			message: 'There seems to be an error. Please try again',
-			id: 'contact-us',
-			message: 'Error!',
-			errors: errors
-		});
-	}
+	// }
+	// else {   //Display errors to user
+	// 	res.send({ 
+	// 		title: 'Contact us',
+	// 		message: 'There seems to be an error. Please try again',
+	// 		id: 'contact-us',
+	// 		message: 'Error!',
+	// 		errors: errors
+	// 	});
+	// }
 });
 
 
