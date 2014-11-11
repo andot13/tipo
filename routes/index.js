b161f3df7ff491a0c0c69414fced075943f67e39
Andy Aparece <andy.aparece@gmail.com>
2014-11-11 15:58:03 +0800

    commented everything in router.post(contact-us)

--- routes/index.js
+++ routes/index.js
@@ -25,60 +25,60 @@ router.get('/contact-us', function(req, res) {
 });
 
 router.post('/contact-us', function(req, res) {
-	req.assert('name', 'Please enter you name.').notEmpty();
-	req.assert('email', 'Please provide a valid email').isEmail();  
+	// req.assert('name', 'Please enter you name.').notEmpty();
+	// req.assert('email', 'Please provide a valid email').isEmail();  
 
-	var errors = req.validationErrors();  
-	if( !errors){   //No errors were found.  Passed Validation!
-		res.redirect('/contact-us');
-		// , { 
-		// 	title: 'Contact us',
-		// 	message: 'Thank you! Your message has been sent!',
-		// 	id: 'contact-us',
-		// 	successClass : 'success',
-		// 	errors: {}
-		// });
+	// var errors = req.validationErrors();  
+	// if( !errors){   //No errors were found.  Passed Validation!
+	// 	res.redirect('/contact-us');
+	// 	// , { 
+	// 	// 	title: 'Contact us',
+	// 	// 	message: 'Thank you! Your message has been sent!',
+	// 	// 	id: 'contact-us',
+	// 	// 	successClass : 'success',
+	// 	// 	errors: {}
+	// 	// });
 
-			var name = req.body.name;
-			var email = req.body.email;
-			var message = req.body.message;
+	// 		var name = req.body.name;
+	// 		var email = req.body.email;
+	// 		var message = req.body.message;
 
-			var message = {
-			    "text": message,
-			    "subject": "optiBPO Contact Form Submission",
-			    "from_email": email,
-			    "from_name": name,
-			    "to": [{
-			            "email": "andotjackass@gmail.com",
-			            "name": "Andy"
-			        }],
-			    "headers": {
-			        "Reply-To": email
-			    },
-			    "track_opens": true,
-			    "track_clicks": true,
-			    "auto_text": true
-			};
+	// 		var message = {
+	// 		    "text": message,
+	// 		    "subject": "optiBPO Contact Form Submission",
+	// 		    "from_email": email,
+	// 		    "from_name": name,
+	// 		    "to": [{
+	// 		            "email": "andotjackass@gmail.com",
+	// 		            "name": "Andy"
+	// 		        }],
+	// 		    "headers": {
+	// 		        "Reply-To": email
+	// 		    },
+	// 		    "track_opens": true,
+	// 		    "track_clicks": true,
+	// 		    "auto_text": true
+	// 		};
 
-			mandrill_client.messages.send({"message": message}, function(result) {
-		    console.log(result);
+	// 		mandrill_client.messages.send({"message": message}, function(result) {
+	// 	    console.log(result);
 		   
-			}, function(e) {
-			    // Mandrill returns the error as an object with name and message keys
-			    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-			    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-			});
-	}
-	else {   //Display errors to user
-		res.redirect('/contact-us');
-		// , { 
-		// 	title: 'Contact us',
-		// 	message: 'There seems to be an error. Please try again',
-		// 	id: 'contact-us',
-		// 	message: 'Error!',
-		// 	errors: errors
-		// });
-	}
+	// 		}, function(e) {
+	// 		    // Mandrill returns the error as an object with name and message keys
+	// 		    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+	// 		    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+	// 		});
+	// }
+	// else {   //Display errors to user
+	// 	res.redirect('/contact-us');
+	// 	// , { 
+	// 	// 	title: 'Contact us',
+	// 	// 	message: 'There seems to be an error. Please try again',
+	// 	// 	id: 'contact-us',
+	// 	// 	message: 'Error!',
+	// 	// 	errors: errors
+	// 	// });
+	// }
 });
 
 
